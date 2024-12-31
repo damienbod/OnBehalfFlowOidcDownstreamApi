e962f0bffc574fa31de6602cafc7593ef2319e0a
damienbod <damien_bod@hotmail.com>
2024-12-31 17:01:02 +0100

    Bootstrap 5

--- MicrosoftEntraID/RazorPage/Pages/Error.cshtml.cs
+++ MicrosoftEntraID/RazorPage/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace RazorPageMicrosoftEntraIDAuth.Pages;
+namespace EntraIdRazorPage.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 public class ErrorModel : PageModel
--- MicrosoftEntraID/RazorPage/Pages/Index.cshtml.cs
+++ MicrosoftEntraID/RazorPage/Pages/Index.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPageMicrosoftEntraIDAuth.Pages;
+namespace EntraIdRazorPage.Pages;
 
 public class IndexModel : PageModel
 {
